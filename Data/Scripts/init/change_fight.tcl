501a71a616d7640611c993d2342cae08c4fb0433
Christian Voß <cech12@gmail.com>
2020-05-10 11:54:27 +0200

    Bugfix for wrong weapon combinations #2

---
+++ Data/Scripts/init/change_fight.tcl
@@ -0,0 +1,13 @@
+$start
+$replace
+fight_weapon_kombi	{ Axt_unq_1 Schild_unq_1 } { 2.0 1.5 }
+$with
+fight_weapon_kombi	{ Axt_unq_1 Schild_unq_2 } { 2.0 1.5 }
+$end
+
+$start
+$replace
+fight_weapon_kombi	{ Axt_unq_3 Schild_unq_2 } { 2.0 1.5 }
+$with
+fight_weapon_kombi	{ Axt_unq_3 Schild_unq_1 } { 2.0 1.5 }
+$end
