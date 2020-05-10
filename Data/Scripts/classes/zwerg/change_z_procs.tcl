7b135f0667be15ba0a26979ec2417e82e1453e5b
Christian Voß <cech12@gmail.com>
2020-05-10 11:40:06 +0200

    Bugfix: Sometimes books did not fly when placed via F9

--- Data/Scripts/classes/zwerg/change_z_procs.tcl
+++ Data/Scripts/classes/zwerg/change_z_procs.tcl
@@ -10,6 +10,10 @@ $replace
 			set_posbottom $invitem [vector_fix $npos]
 			from_wall $item
 $with
-			set_posbottom $invitem [vector_fix $npos]
+			if {[get_objclass $invitem] == "Schatzbuch"} {
+				call_method $invitem initiate [vector_fix $npos]
+			} else {
+				set_posbottom $invitem [vector_fix $npos]
+			}
 			from_wall $invitem
 $end
