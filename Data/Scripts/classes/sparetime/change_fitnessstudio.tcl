f85916eb7127a71c6456e6b1568cc15521dbd5ad
Sebastian Weiser <github-projects@sweiser.33mail.com>
2024-06-15 21:04:46 +0200

    minimized changeset for the fix

--- Data/Scripts/classes/sparetime/change_fitnessstudio.tcl
+++ Data/Scripts/classes/sparetime/change_fitnessstudio.tcl
@@ -1,18 +1,6 @@
 $start
 $replace
-	obj_init {
-		set_anim this fitnessstudio.standard 0 $ANIM_LOOP
-		call scripts/misc/genericprod.tcl
 		set_energyconsumption this 2
-		set_collision this 1
-
-		prod_guest addlink this 8
 $with
-	obj_init {
-		set_anim this fitnessstudio.standard 0 $ANIM_LOOP
-		call scripts/misc/genericprod.tcl
 		set_energyconsumption this 0
-		set_collision this 1
-
-		prod_guest addlink this 8
 $end
