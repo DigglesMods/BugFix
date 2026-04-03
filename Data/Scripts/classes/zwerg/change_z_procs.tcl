68dfd0879496f66e34fbade5b83b79a01a502543
saibantes <38582285+saibantes@users.noreply.github.com>
2026-04-03 08:06:38 +0200

    Merge pull request #26
    * bug fix: When a lot of items lay on the ground, gnomes may fail to pu…
    
    * Merge branch 'master' into fix-put-down

--- Data/Scripts/classes/zwerg/change_z_procs.tcl
+++ Data/Scripts/classes/zwerg/change_z_procs.tcl
@@ -17,3 +17,33 @@ $with
 			}
 			from_wall $invitem
 $end
+$start
+$replace
+	} else {
+		// putdown an bestimmter Position
+
+		tasklist_add this "set_objworkicons this arrow_down [get_objclass $item]; walk_near_item \{$pos\} 0.7"
+		tasklist_add this "rotate_towards \{$pos\}"
+		tasklist_add this "play_anim benda"
+		tasklist_add this "beam_from_inv_to_pos \{$item\} \{$pos\}"
+$with
+	} else {
+		// putdown an bestimmter Position
+
+		tasklist_add this "set_objworkicons this arrow_down [get_objclass $item]; walk_near_item \{$pos\} 0.7 0.1 auto 1.25"
+		tasklist_add this "rotate_towards \{$pos\}"
+		tasklist_add this "play_anim benda"
+		tasklist_add this "beam_from_inv_to_pos \{$item\} \{$pos\}"
+$end
+$start
+$replace
+		tasklist_addfront this "beam_from_inv_to_pos \{$item\} \{$pos\}"
+		tasklist_addfront this "play_anim benda"
+		tasklist_addfront this "rotate_towards \{$pos\}"
+		tasklist_addfront this "set_objworkicons this arrow_down [get_objclass $item]; walk_near_item \{$pos\} 0.7"
+$with
+		tasklist_addfront this "beam_from_inv_to_pos \{$item\} \{$pos\}"
+		tasklist_addfront this "play_anim benda"
+		tasklist_addfront this "rotate_towards \{$pos\}"
+		tasklist_addfront this "set_objworkicons this arrow_down [get_objclass $item]; walk_near_item \{$pos\} 0.7 0.1 auto 1.25"
+$end
--- Data/Scripts/classes/zwerg/change_z_work_prod.tcl
+++ Data/Scripts/classes/zwerg/change_z_work_prod.tcl
@@ -1,5 +1,59 @@
 $start
 $replace
+	tasklist_addfront this "beam_from_inv_to_pos $item \{$itempos\};play_anim bendb"
+	tasklist_addfront this "play_anim benda"
+	tasklist_addfront this "rotate_towards \{$itempos\}"
+	tasklist_addfront this "walk_near_item \{$itempos\} 0.7"
+
+	return true
+}
+$with
+	tasklist_addfront this "beam_from_inv_to_pos $item \{$itempos\};play_anim bendb"
+	tasklist_addfront this "play_anim benda"
+	tasklist_addfront this "rotate_towards \{$itempos\}"
+	tasklist_addfront this "walk_near_item \{$itempos\} 0.7 0.1 auto 1.25"
+
+	return true
+}
+$end
+$start
+$replace
+	tasklist_addfront this "beam_from_inv_to_pos $item \{$itempos\};play_anim bendb"
+	tasklist_addfront this "play_anim benda"
+	tasklist_addfront this "rotate_towards \{$itempos\}"
+	tasklist_addfront this "walk_near_item \{$itempos\} 0.7"
+	return true
+}
+
+$with
+	tasklist_addfront this "beam_from_inv_to_pos $item \{$itempos\};play_anim bendb"
+	tasklist_addfront this "play_anim benda"
+	tasklist_addfront this "rotate_towards \{$itempos\}"
+	tasklist_addfront this "walk_near_item \{$itempos\} 0.7 0.1 auto 1.25"
+	return true
+}
+
+$end
+$start
+$replace
+			return false
+		}
+	}
+	tasklist_add this "walk_near_item \{$itempos\} 0.7"
+	tasklist_add this "rotate_towards \{$itempos\}"
+	tasklist_add this "play_anim benda" ;# prod_gnome_state this putdown $item
+	tasklist_add this "beam_from_inv_to_pos $item \{$itempos\};set_roty $item [random 6.3];play_anim bendb"
+$with
+			return false
+		}
+	}
+	tasklist_add this "walk_near_item \{$itempos\} 0.7 0.1 auto 1.25"
+	tasklist_add this "rotate_towards \{$itempos\}"
+	tasklist_add this "play_anim benda" ;# prod_gnome_state this putdown $item
+	tasklist_add this "beam_from_inv_to_pos $item \{$itempos\};set_roty $item [random 6.3];play_anim bendb"
+$end
+$start
+$replace
 // item passt nicht in die Tasche --> fertig"
 $with
 // item passt nicht in die Tasche --> fertig
